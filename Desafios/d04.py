11c2d9b86e79ccd151bb9cc5bbbe8f6ed41448f3
Guilh-Code <GuilhermeRAR2005@gmail.com>
2025-04-15 00:11:47 -0300

    dicas de .is

--- Desafios/d04.py
+++ Desafios/d04.py
@@ -1,9 +1,9 @@
 a = input('Diga algo: ')
-print('O tipo primitivo desse valor é', type(a))
-print('Só tem espaços? ', a.isspace())
-print('É um número? ', a.isnumeric())
-print('É alfabético? ', a.isalpha())
-print('É alfanumérico? ', a.isalnum())
-print('Está em maiúsculas? ', a.isupper())
-print('Está em minúsculas? ', a.islower())
-print('Está capitalizada? ', a.istitle())
+print('O tipo primitivo desse valor é', type(a)) #Mostra o Tipo Primitivo do valor digitado
+print('Só tem espaços? ', a.isspace())      #Mostra se foi digitado em espaços
+print('É um número? ', a.isnumeric())       #Mostra se foi digitado um número
+print('É alfabético? ', a.isalpha())        #Mostra se foi digitado uma palavra/letra
+print('É alfanumérico? ', a.isalnum())      #Mostra que foi digitado um número e uma letra
+print('Está em maiúsculas? ', a.isupper())  #Mostra que o texto digitado foi em MAIÚSCULO
+print('Está em minúsculas? ', a.islower())  #Mostra que o texto digitado foi em MINÚSCULO
+print('Está capitalizada? ', a.istitle()) #Mostra que a palavra digitada foi em formato de Título
