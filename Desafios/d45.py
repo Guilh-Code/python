3c84bcbd02f007b5e58fb0ee56866f23028d27c5
Guilh-Code <GuilhermeRAR2005@gmail.com>
2025-04-22 19:42:57 -0300

    resolvido problema do d45

--- Desafios/d45.py
+++ Desafios/d45.py
@@ -5,68 +5,63 @@
 from random import randint
 from time import sleep
 
-# Define as opções possíveis do jogo em uma tupla
-itens = ('Pedra', 'Papel', 'Tesoura')
+# Define as opções do jogo
+opcoes = ('Pedra', 'Papel', 'Tesoura')
 
-# O computador escolhe aleatoriamente um número entre 0 e 2 (correspondente a Pedra, Papel ou Tesoura)
-computador = randint(0, 2)
+computador = randint(0, 2) # O computador escolhe aleatoriamente um número entre 0 e 2 (correspondente a Pedra, Papel ou Tesoura)
 
-# Mostra as opções disponíveis para o jogador
-print(''' SUAS OPÇÔES:
-    [ 0 ] PEDRA
-    [ 1 ] PAPEL
-    [ 2 ] TESOURA''')
+# Mostra o menu para o jogador
+print('''SUAS OPÇÕES:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')
 
-# Recebe a escolha do jogador como um número inteiro
+# Solicita a jogada do jogador
 jogador = int(input('Qual é a sua jogada? '))
 
-# Faz a contagem dramática do "JO-KEN-PO"
-print('JO')
-sleep(1)  # Espera 1 segundo
-print('KEN')
-sleep(1)
-print('PO!!!')
-sleep(1)
+# Verifica se a jogada é válida
+if jogador < 0 or jogador > 2:
+    print('\nJOGADA INVÁLIDA! Por favor, escolha entre 0, 1 ou 2.')
+else:
+    # Animação do "JO-KEN-PO"
+    print('\nJO')
+    sleep(1)
+    print('KEN')
+    sleep(1)
+    print('PO!!!\n')
+    sleep(0.5)
 
-# Imprime uma linha de separação
-print('-=-' * 12)
+    # Mostra a jogada do computador e do jogador com base no índice da tupla
+    print('-=-' * 12)
+    print(f'Computador jogou {opcoes[computador]}')
+    print(f'Jogador jogou {opcoes[jogador]}')
+    print('-=-' * 12)
 
-# Mostra a jogada do computador e do jogador com base no índice da tupla
-print(f'Computador jogou {itens[computador]}')
-print(f'Jogador jogou {itens[jogador]}')
-print('-=-' * 12)
+    # Lógica para determinar o vencedor com base nas escolhas
 
-# Lógica para determinar o vencedor com base nas escolhas
+    # Se o computador jogou PEDRA
+    if computador == 0:
+        if jogador == 0:
+            print('EMPATE!')
+        elif jogador == 1:
+            print('JOGADOR VENCE!')
+        elif jogador == 2:
+            print('COMPUTADOR VENCE!')
 
-# Se o computador jogou PEDRA
-if computador == 0:
-    if jogador == 0:
-        print('EMPATE')  # Ambos jogaram Pedra
-    elif jogador == 1:
-        print('JOGADOR VENCE')  # Papel vence Pedra
-    elif jogador == 2:
-        print('COMPUTADOR VENCE')  # Pedra vence Tesoura
-    else:
-        print('JOGADA INVÁLIDA!')
+    # Se o computador jogou PAPEL
+    elif computador == 1:
+        if jogador == 0:
+            print('COMPUTADOR VENCE!')
+        elif jogador == 1:
+            print('EMPATE!')
+        elif jogador == 2:
+            print('JOGADOR VENCE!')
 
-# Se o computador jogou PAPEL
-elif computador == 1:
-    if jogador == 0:
-        print('COMPUTADOR VENCE')  # Papel vence Pedra
-    elif jogador == 1:
-        print('EMPATE')  # Ambos jogaram Papel
-    elif jogador == 2:
-        print('JOGADOR VENCE')  # Tesoura vence Papel
-    else:
-        print('JOGADA INVÁLIDA!')
-
-# Se o computador jogou TESOURA
-elif computador == 2:
-    if jogador == 0:
-        print('JOGADOR VENCE')  # Pedra vence Tesoura
-    elif jogador == 1:
-        print('COMPUTADOR VENCE')  # Tesoura vence Papel
-    elif jogador == 2:
-        print('EMPATE')  # Ambos jogaram Tesoura
-    else:
-        print('JOGADA INVÁLIDA!')
+    # Se o computador jogou TESOURA
+    elif computador == 2:
+        if jogador == 0:
+            print('JOGADOR VENCE!')
+        elif jogador == 1:
+            print('COMPUTADOR VENCE!')
+        elif jogador == 2:
+            print('EMPATE!')
